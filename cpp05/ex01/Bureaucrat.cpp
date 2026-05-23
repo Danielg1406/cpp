3832ec3ba72e21d1a71cea64c339cb081bc606fa
Danielg1406 <danielgomeza1406@gmail.com>
2026-05-23 12:14:35 +0200

    fix: update Bureaucrat class for const

--- cpp05/ex01/Bureaucrat.cpp
+++ cpp05/ex01/Bureaucrat.cpp
@@ -1,18 +1,20 @@
 #include "Bureaucrat.hpp"
 
+#include "Form.hpp"
+
 // Exception Message
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
-   return "Grade too high!\n";
+	return "Grade too high!";
 }
 
 const char* Bureaucrat::GradeTooLowException::what() const throw() {
-   return "Grade too low!\n";
+	return "Grade too low!";
 }
 
 // Canonical Form
 Bureaucrat::Bureaucrat(): _name("New"), _grade(150){}
 
-Bureaucrat::Bureaucrat(const std::string name, int grade): _name(name) {
+Bureaucrat::Bureaucrat(const std::string &name, int grade): _name(name) {
 	if (grade > 150)
 		throw GradeTooLowException();
 	if (grade < 1)
@@ -20,9 +22,7 @@ Bureaucrat::Bureaucrat(const std::string name, int grade): _name(name) {
 	this->_grade = grade;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &other) {
-	*this = other;
-}
+Bureaucrat::Bureaucrat(const Bureaucrat &other): _name(other._name), _grade(other._grade) {}
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other) {
 	if (this != &other)
@@ -33,7 +33,7 @@ Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other) {
 Bureaucrat::~Bureaucrat() {}
 
 // Getters
-std::string	Bureaucrat::getName() const{
+const std::string	&Bureaucrat::getName() const{
 	return this->_name;
 }
 
@@ -52,6 +52,15 @@ void		Bureaucrat::decrementGrade() {
 	if (this->_grade - 1 < 1)
 		throw GradeTooHighException();
 	this->_grade--;
+}
+
+void		Bureaucrat::signForm(Form &form) {
+	try {
+		form.beSigned(*this);
+		std::cout << this->getName() << " signed " << form.getName() << std::endl;
+	} catch (std::exception &e) {
+		std::cout << this->getName() << " couldn't sign " << form.getName() << " because " << e.what() << std::endl;
+	}
 }
 
 // Operator Overload
--- cpp05/ex01/Bureaucrat.hpp
+++ cpp05/ex01/Bureaucrat.hpp
@@ -20,19 +20,20 @@
 class Bureaucrat 
 {
 	private:
-		std::string	_name;
+		const std::string	_name;
 		int		_grade;
 	public:
 		Bureaucrat();
-		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const std::string &name, int grade);
 		Bureaucrat(const Bureaucrat &other);
 		Bureaucrat& operator=(const Bureaucrat &other);
 		~Bureaucrat();
 
-		std::string	getName() const;
+		const std::string&	getName() const;
 		int		getGrade() const;
 		void		incrementGrade();
 		void		decrementGrade();
+		void		signForm(class Form &form);
 
 		class GradeTooHighException : public std::exception {
 			public:
