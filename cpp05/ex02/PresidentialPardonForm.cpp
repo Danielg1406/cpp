85cd1d9c8828308626e93421f2564b3130c77c5f
Danielg1406 <danielgomeza1406@gmail.com>
2026-05-23 17:00:30 +0200

    feat: add PresidentialPardonForm class

---
+++ cpp05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,23 @@
+#include "PresidentialPardonForm.hpp"
+#include <iostream>
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target)
+    : AForm("PresidentialPardon", 25, 5), _target(target) {}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other)
+    : AForm(other), _target(other._target) {}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &other) {
+    if (this != &other) 
+    {
+        AForm::operator=(other);
+        this->_target = other._target;
+    }
+    return *this;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm() {}
+
+void PresidentialPardonForm::action() const {
+    std::cout << _target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
