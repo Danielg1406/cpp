0d606b0b89f5c6d00df5259ee91922ef1ea41d41
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2026-05-28 19:28:01 +0200

    fix: make iter size const

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -4,19 +4,19 @@
 #include <cstddef>
 
 template <typename T>
-void iter(T *array, std::size_t length, void (*function)(T &)) {
+void iter(T *array, const std::size_t length, void (*function)(T &)) {
 	for (std::size_t index = 0; index < length; ++index)
 		function(array[index]);
 }
 
 template <typename T>
-void iter(T *array, std::size_t length, void (*function)(const T &)) {
+void iter(T *array, const std::size_t length, void (*function)(const T &)) {
 	for (std::size_t index = 0; index < length; ++index)
 		function(array[index]);
 }
 
 template <typename T>
-void iter(const T *array, std::size_t length, void (*function)(const T &)) {
+void iter(const T *array, const std::size_t length, void (*function)(const T &)) {
 	for (std::size_t index = 0; index < length; ++index)
 		function(array[index]);
 }
